6d7aea916073501c11e82374e76b05069664acca
IlliaKlason <105161428+IlliaKlason@users.noreply.github.com>
2022-08-28 01:00:50 +0300

    Summary pagination

---
+++ src/js/pagination.js
@@ -0,0 +1,16 @@
+import Pagination from 'tui-pagination';
+import 'tui-pagination/dist/tui-pagination.css';
+
+const pageMenu = function (total) {
+   const container = document.getElementById('pagination');
+   const options = {
+      totalItems: total,
+      itemsPerPage: 16,
+      visiblePages: 5,
+      page: 1,
+      centerAlign: true,
+   };
+   const pagination = new Pagination(container, options);
+   return pagination;
+};
+pageMenu(200)
