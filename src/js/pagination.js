f9fa6db6d631d9883409e87197056f5d63cb8714
IlliaKlason <105161428+IlliaKlason@users.noreply.github.com>
2022-08-29 21:14:10 +0300

    Summary

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -15,18 +15,3 @@ export const pageMenu = function (total) {
 
   return pagination;
 };
-
-pageMenu(100)
-
-// const pagination = pageMenu(data.page.totalElements / 16);
-// pagination.on('beforeMove', async function (eventData) {
-//   const page = eventData.page;
-//   try {
-//     const { data } = await fetchCardsByName(query, locale, page);
-//     const result = data._embedded;
-//     conteinerEl.innerHTML = cardsRender(result.events);
-//   } catch (err) {
-//     console.log(err);
-//   }
-// });
-
