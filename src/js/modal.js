59c18eaca582d51ef3abac9154477d38ef4b16d2
iboks77 <iboks77@gmail.com>
2022-08-29 19:35:00 +0300

    add add

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,12 +1,17 @@
-import { fetchServer } from './fetch-event';
+import { getFromSS } from './utils';
+import { key } from './fetch-event';
 
 const closeModalBtn = document.querySelector('[data-modal-close]');
 const backdropModal = document.querySelector('[data-modal]');
+
 closeModalBtn.addEventListener('click', toggleModal);
 
 function toggleModal() {
-  backdropModal.classList.toggle('is-hidden');
+  backdropModal.classList.toggle('visually-hidden');
 }
+
+const x = getFromSS(key);
+console.log(x);
 
 // const BASE_URL = 'https://app.ticketmaster.com/discovery/v2/events';
 // const API_KEY = 'unEzXyPGRdZtlW4MZOT74rfieLb91xjQ';
