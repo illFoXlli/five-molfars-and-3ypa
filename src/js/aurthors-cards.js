38eceb52d9c22fd74dde99f55d4ebba308ee4bdc
Roman <roman.markanych@gmail.com>
2022-08-30 22:16:56 +0300

    3

--- src/js/aurthors-cards.js
+++ src/js/aurthors-cards.js
@@ -1,6 +1,7 @@
 import { fetchServer } from './fetch-event';
-import { toggleModal } from './modal';
-import { saveToSS } from './utils';
+import { toggleModal, getCard, markupModal } from './modal';
+import { saveToSS, getFromSS } from './utils';
+import { key } from './fetch-event';
 
 const aboutAuthorBtn = document.querySelector('.modal__list-btn-more');
 
@@ -9,4 +10,12 @@ aboutAuthorBtn.addEventListener('click', onAboutAuthorBtnClick);
 function onAboutAuthorBtnClick() {
   toggleModal();
 }
-let author = rec.data._embeded.venue;
+
+// let result = getCard();
+// console.log(result._embedded.venues.name);
+
+// let result1 = getFromSS(key);
+// console.log(result1._embedded.venues.name);
+// console.log(result1);
+
+//let author = rec.data._embedded.venues.name;
