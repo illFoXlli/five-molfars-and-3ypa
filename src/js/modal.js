a04b756298b13df42c513ea8f12599bcea040c5e
Denis <fox.ruden@gmail.com>
2022-08-31 23:51:37 +0300

    save

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,63 +1,77 @@
-import { getFromSS } from './utils';
+import { getFromSS, saveToSS } from './utils';
 import { key } from './fetch-event';
+import btnModal from '../templates/btn-modal.hbs';
 
-let closeModalBtn = document.querySelector('[data-modal-close]');
+let closeModalClickBtn = document.querySelector('[data-modal-close]');
 const backdropModal = document.querySelector('[data-modal]');
-const boxOnClick = document.querySelector('.events');
+const onBoxClick = document.querySelector('.events');
+const btnBuy = document.querySelector('.btnBuy');
 let findUl;
 let getDataSS;
 let getCards;
+let idCard;
 export let authorName;
+const modalLiBtn = document.querySelector('.modal--prise');
+const modalLih2 = document.querySelector('.modal__list-title--modal');
 
-closeModalBtn.addEventListener('click', toggleModal);
-backdropModal.addEventListener('click', closeModal);
-boxOnClick.addEventListener('click', onCardClick);
+closeModalClickBtn.addEventListener('click', onToggleModalClick);
+backdropModal.addEventListener('click', onCloseModalClick);
+onBoxClick.addEventListener('click', onCardClick);
+document.addEventListener('keydown', onCloseByKeyKeydown);
 
-export function toggleModal(e) {
+function getCard(getCards, findUl) {
+  let filterCard = getCards.find(card => card.id === findUl);
+  filterCard.images = filterCard.images.sort((a, b) => b.width - a.width);
+  markupModal(filterCard);
+  authorName = filterCard._embedded.venues[0].name;
+  saveToSS(findUl, filterCard);
+}
+
+export function onToggleModalClick(e) {
   backdropModal.classList.toggle('is-hidden');
   document.body.classList.remove('no-scroll');
 }
 
-function closeModal(e) {
+function onCloseModalClick(e) {
   if (e.target === e.currentTarget) {
     backdropModal.classList.toggle('is-hidden');
     document.body.classList.remove('no-scroll');
   }
 }
-document.addEventListener('keydown', closeByKey);
 
-function closeByKey(e) {
+function onCloseByKeyKeydown(e) {
   if (e.code !== 'Escape') {
     return;
   } else {
     backdropModal.classList.toggle('is-hidden');
     document.body.classList.remove('no-scroll');
-    document.removeEventListener('keydown', closeByKey);
+    document.removeEventListener('keydown', onCloseByKeyKeydown);
   }
 }
 
 function onCardClick(event) {
-  document.addEventListener('keydown', closeByKey);
+  document.addEventListener('keydown', onCloseByKeyKeydown);
 
   getDataSS = getFromSS(key);
   getCards = getDataSS.data._embedded.events;
   if (event.target.nodeName !== 'DIV') {
     findUl = event.target.closest('ul').id;
+    idCard = findUl;
     backdropModal.classList.toggle('is-hidden');
     document.body.classList.add('no-scroll');
     getCard(getCards, findUl);
   }
+
+  let cardModal = getFromSS(findUl);
+  console.log(cardModal);
+  let linc = cardModal.url;
+  console.log('+++++++++++++TUT++++++++++');
+  console.log(cardModal.url);
+  modalLih2.innerHTML = btnModal(cardModal);
 }
 
-function getCard(getCards, findUl) {
-  let filterCard = getCards.find(card => card.id === findUl);
-  console.log(filterCard);
-  filterCard.images = filterCard.images.sort((a, b) => b.width - a.width);
-  markupModal(filterCard);
-  authorName = filterCard._embedded.venues[0].name;
-}
-function markupModal({ images, info, priceRanges, dates, _embedded, url }) {
-  let img = document.querySelector('.modal img.modal__img');
+function markupModal({ images, info, priceRanges, dates, _embedded }) {
+  let img = document.querySelector('.modal__img');
   img.src = images[0].url;
   let infoWhen = document.querySelector('.whenDate');
   infoWhen.textContent = `${dates.start.localDate}`;
@@ -82,6 +96,7 @@ function markupModal({ images, info, priceRanges, dates, _embedded, url }) {
   }
 
   let infoText = document.querySelector('.modal p.modal__list-text');
+
   try {
     if (info === undefined) {
       infoText.textContent = accessibility.info;
@@ -90,6 +105,7 @@ function markupModal({ images, info, priceRanges, dates, _embedded, url }) {
   } catch {
     infoText.textContent = 'No text 🍲';
   }
+
   try {
     let infoPrice = document.querySelector('.modal span.modal__standart');
     infoPrice.textContent = `${priceRanges[0].type.toUpperCase()} ${
@@ -98,10 +114,13 @@ function markupModal({ images, info, priceRanges, dates, _embedded, url }) {
   } catch {
     console.log('Price is not defined');
   }
-  let btnBuy = document.querySelector('.btnBuy');
-  btnBuy.addEventListener('click', onBuyTicketClick);
 
-  function onBuyTicketClick() {
-    return btnBuy.setAttribute('href', `${url}`);
-  }
+  // let btnBuy = document.querySelector('.btnBuy');
+  // btnBuy.addEventListener('click', onBuyTicketClick);
+
+  // function onBuyTicketClick() {
+  //   return btnBuy.setAttribute('href', `${url}`);
+  // }
 }
+
+// modalLiBtn.insertAdjacentHTML('beforeend', btnModal(filterCard));
